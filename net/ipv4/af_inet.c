34d14d1046e44752e7783e8d391e174673ef1333
Andi Kleen <ak@linux.intel.com>
2014-01-13 13:09:23 -0800

    net, diet: Make MIB statistics collections depend on PROC_FS
    When PROC_FS is not compiled in we don't need the statistics
    gathering code, as the only way to see the output is through
    /proc. Saves about 5k text, likely more in dynamic memory.
    
       text    data     bss     dec     hex filename
     386302    8993   12564  407859   63933 net/built-in.o-with-mib
     381542    8933   12564  403039   6265f net/built-in.o-wo-mib
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>
    [Fixed rejects and more]
    Signed-off-by: Tom Zanussi <tom.zanussi@linux.intel.com>

--- net/ipv4/af_inet.c
+++ net/ipv4/af_inet.c
@@ -1459,6 +1459,8 @@ int inet_ctl_sock_create(struct sock **sk, unsigned short family,
 }
 EXPORT_SYMBOL_GPL(inet_ctl_sock_create);
 
+#ifdef CONFIG_PROC_FS
+
 unsigned long snmp_fold_field(void __percpu *mib, int offt)
 {
 	unsigned long res = 0;
@@ -1497,6 +1499,8 @@ u64 snmp_fold_field64(void __percpu *mib, int offt, size_t syncp_offset)
 EXPORT_SYMBOL_GPL(snmp_fold_field64);
 #endif
 
+#endif
+
 #ifdef CONFIG_IP_MULTICAST
 static const struct net_protocol igmp_protocol = {
 	.handler =	igmp_rcv,
@@ -1528,6 +1532,7 @@ static const struct net_protocol icmp_protocol = {
 	.netns_ok =	1,
 };
 
+#ifdef CONFIG_PROC_FS
 static __net_init int ipv4_mib_init_net(struct net *net)
 {
 	int i;
@@ -1601,6 +1606,13 @@ static int __init init_ipv4_mibs(void)
 {
 	return register_pernet_subsys(&ipv4_mib_ops);
 }
+
+#else
+static int __init init_ipv4_mibs(void)
+{
+	return 0;
+}
+#endif
 
 static __net_init int inet_init_net(struct net *net)
 {
