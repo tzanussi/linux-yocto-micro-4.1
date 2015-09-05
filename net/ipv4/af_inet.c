46bf7b68001250998223dd572a5960972333eb48
Tom Zanussi <tom.zanussi@linux.intel.com>
2015-09-05 11:18:49 -0500

    fixup! net, diet: Make ping sockets optional
    Fix undefined reference to `pingv6_ops' in tcp_recvmsg().
    
    Signed-off-by: Tom Zanussi <tom.zanussi@linux.intel.com>

--- net/ipv4/af_inet.c
+++ net/ipv4/af_inet.c
@@ -1390,7 +1390,7 @@ int inet_recv_error(struct sock *sk, struct msghdr *msg, int len, int *addr_len)
 {
 	if (sk->sk_family == AF_INET)
 		return ip_recv_error(sk, msg, len, addr_len);
-#if IS_ENABLED(CONFIG_IPV6)
+#if IS_ENABLED(CONFIG_IPV6) && IS_ENABLED(CONFIG_IP_PING)
 	if (sk->sk_family == AF_INET6)
 		return pingv6_ops.ipv6_recv_error(sk, msg, len, addr_len);
 #endif
